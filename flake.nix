7a4fad27d98e8b88076764c09dc95f627a62bfb9
campbellcole <10430178+campbellcole@users.noreply.github.com>
2023-01-08 20:45:40 -0800

    actually remove unnecessary dependencies

--- flake.nix
+++ flake.nix
@@ -1,18 +1,16 @@
 {
-  description = "";
+  description = "quick documentation server for dioxus 0.3.x docs";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
       in
       with pkgs;
